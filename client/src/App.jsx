16023bae088cfe12d16f5de1f1e721b3da051cc3
Kishor <kishorjsk2006@gmail.com>
2025-04-12 20:15:22 +0530

    Created A Simple Dashboard On The Front Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import Sidebar from './components/Sidebar.jsx'
+import Dashboard from './components/Dashboard.jsx'
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Router>
       <Header />
       <Sidebar />
+      <Dashboard />
       </Router>
   );
 }
