4f52039873a45fda84486125f6a957cda3695756
Kishor <kishorjsk2006@gmail.com>
2025-04-12 17:17:22 +0530

    Changed The Header Content

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Header() {
     return (
         <header className="header">
-        <h1>MurP Murmur your project ideas</h1>
+        <h1>.,.MurP.,.</h1>
         <nav>
             <Link to="/">Dashboard</Link>
             <Link to="/tasks">Tasks</Link>
