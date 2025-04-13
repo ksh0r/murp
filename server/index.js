40316fb6fea5b8e939c4e126b4c525902bcbe2f8
Kishor <kishorjsk2006@gmail.com>
2025-04-13 16:46:57 +0530

    Remade The Sample Server Template, Work In Progress

--- server/index.js
+++ server/index.js
@@ -1,40 +1,12 @@
-/* Comments have been done by ksh0r
- * for better understanding */
+import express from 'express'
 
-import cors from 'cors';
-import dotenv from 'dotenv';
-import express from 'express';
-
-// Call to read .env file. Should be above all other comments.
-dotenv.config();
-
-// Make the express head function to utilize various funtions within it.
 const app = express();
+const PORT = process.env.PORT | 5000;
 
-// Get the port from .env file if exists otherwise sets to default 5000.
-const PORT = process.env.PORT || 5000;
-
-// Make the app utilize the middleware cors.
-app.use(cors());
-
-// Tell the express app to parse browser requests as json and not use the raw data sent out.
-app.use(express.json());
-
-// Request handling check on connecting to localhost:5000.
 app.get('/', (req,res) => {
-        res.send("API is working");
-});
-
-// Dummy functions to understand
-app.get('/power', (req,res) => {
-    res.send("You are powerful");
-});
-app.get('/greet/:name', (req,res) => {
-    const name = req.params.name;
-    res.send(`Hello ${name}`);
+    res.send("Welcome to the MurP Server");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running successfully on ${PORT}`);
-});
-
+    console.log(`The Server is up and running on PORT: ${PORT}`);
+})
