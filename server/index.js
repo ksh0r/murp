7035b52a13101058b893d14ce63d952712f16842
Kishor <kishorjsk2006@gmail.com>
2025-04-14 11:36:18 +0530

    Added A MongoDB Setup On index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
-import express from 'express'
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT | 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.error("MongoDB not connected Error: ", err));
 
 app.get('/', (req,res) => {
     res.send("Welcome to the MurP Server");
